9c10805eee99644c8ce2c467f53cf0ebb38734f4
abbosxon <abbosxon.oripov@yandex.ru>
2022-04-20 16:31:34 +0500

    add exam

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// 1
+let arr = ["I", "study", "JavaScript", "right", "now"]
+let cutter = arr.splice(-3, 1)
+console.log(arr);
+
+
+
+
+// 2---
+// let text = 'salom dunyo';
+
+// function reversing(sum) {
+
+// }
+
+// reversing(text)
+
+// 3---
+
+let number = 4;
+
+function test(num) {
+    if (num % 2 === 0) {
+        return true;
+    }
+    return false
+}
+
+console.log(test(number));
